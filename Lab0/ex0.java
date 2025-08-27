4cfb454ce95410c90a3fabb394552bb3590ba84e
McKenna C. Peace <87356606+anvpeace@users.noreply.github.com>
2025-08-27 00:35:03 +0000

    ex1 practice

--- Lab0/ex0.java
+++ Lab0/ex0.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ex0{
 
     public static void main(String[] arg){
@@ -10,6 +12,12 @@ public class ex0{
         name = "Peace";
 
         System.out.println(intro + name);
+        System.out.print(intro + name); // the print doesnt add a new line
+        Scanner input = new Scanner(System.in);
         
+        String enterName = input.next();
+
+        System.out.println("Enter your name: " + enterName);
+
     };
 }
