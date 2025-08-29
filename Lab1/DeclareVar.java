34cb5b63eadf749db0992703e8a0dbb02a70f0c7
McKenna C. Peace <87356606+anvpeace@users.noreply.github.com>
2025-08-29 00:22:49 +0000

    updates

--- Lab1/DeclareVar.java
+++ Lab1/DeclareVar.java
@@ -1,16 +1,32 @@
 public class DeclareVar{
     static public void main(String[] args){
 
-        String name = "McKenna C Peace";
+        String name;
+
+        name = "Mike";
+
+        String name2 = "Mike";
 
         // prints third letter of string 'name'
         System.out.println(name.charAt(2));
         System.out.println("Hello\nWorld\tHi,\tPeace");
 
+        for(int i= 0; i<name.length(); i++){
+            System.out.println(name);
+        }
 
+        
+                
+        System.out.println("Line 1" + name); //.println() = after this line is executed start the next line of code on a new line
+        System.out.print("Line 2" + name); //.print() = after this line of code is executed dont start a new line. 
+        System.out.println("Line 3" + name);
+
+
+        // System.out.println(name.charAt(2) + name.charAt(4));
 
 
 
 
     }
 }
+
